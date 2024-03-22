73b46d47dbb9412fca05200d541ffe5a3a9dbcfc
Andrei Achim <andrei.bogdanel.achim@gmail.com>
2024-03-22 18:01:17 +0200

    recursively handle directories

--- src/dir.rs
+++ src/dir.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{ffi::OsString, fs};
 
 use crate::{source_dir::SourceDir, target_dir::TargetDir};
 
@@ -10,26 +10,21 @@ pub struct Dir<'a> {
 
 impl<'a> Dir<'a> {
     pub fn process(&self) -> anyhow::Result<()> {
-        let parsed_dir = self.parse()?;
-        Ok(())
-    }
-
-    fn parse(&self) -> anyhow::Result<ParsedDir> {
-        let mut dirs: Vec<String> = vec![];
+        let mut dirs: Vec<OsString> = vec![];
         let mut pages: Vec<String> = vec![];
-        let mut inner_template: Option<String> = None;
+        let mut layout: Option<String> = None;
 
         fs::read_dir(self.source_directory.get_path())?
             .flat_map(|r| r.ok())
             .for_each(|dir_entry| {
                 if dir_entry.path().is_dir() {
-                    dirs.push(dir_entry.path().to_string_lossy().to_string());
+                    dirs.push(dir_entry.file_name());
                 }
 
                 if dir_entry.path().is_file() {
                     match dir_entry.file_name().to_string_lossy() {
                         s if s.ends_with("layout.html") => {
-                            inner_template = fs::read_to_string(dir_entry.path()).ok();
+                            layout = self.merge_layouts(fs::read_to_string(dir_entry.path()).ok());
                         }
                         s if s.ends_with("page.html") => {
                             if let Ok(page_content) = fs::read_to_string(dir_entry.path()) {
@@ -41,18 +36,40 @@ impl<'a> Dir<'a> {
                 }
             });
 
-        Ok(ParsedDir {
-            inherited_template: self.inherited_template.clone(),
-            inner_template,
-            pages,
-            dirs,
-        })
-    }
-}
+        self.process_dirs(dirs, layout)?;
 
-struct ParsedDir {
-    inherited_template: Option<String>,
-    inner_template: Option<String>,
-    pages: Vec<String>,
-    dirs: Vec<String>,
+        Ok(())
+    }
+
+    fn merge_layouts(&self, inner_layout: Option<String>) -> Option<String> {
+        let inherited_layout = self.inherited_template.clone();
+        inner_layout.map_or_else(
+            || inherited_layout.clone(),
+            |inner| {
+                Some(
+                    inherited_layout
+                        .clone()
+                        .map(|inherited| "hello".to_string())
+                        .unwrap_or(inner),
+                )
+            },
+        )
+    }
+
+    fn process_dirs(&self, dirs: Vec<OsString>, layout: Option<String>) -> anyhow::Result<()> {
+        for dir in dirs {
+            let source_directory = self.source_directory.get_path().join(&dir);
+            let source_directory = SourceDir::new(&source_directory)?;
+            let target_directory = self.target_directory.get_path().join(dir);
+            let target_directory = TargetDir::new(&target_directory, false)?;
+
+            Dir {
+                source_directory,
+                target_directory,
+                inherited_template: layout.clone(),
+            }
+            .process()?;
+        }
+        Ok(())
+    }
 }
